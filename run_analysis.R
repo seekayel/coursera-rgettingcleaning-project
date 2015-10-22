be0ec8f73be1656e4362087453c9a4c60a72be43
Kam Lasater <ckl@seekayel.com>
2015-10-22 19:16:32 -0400

    Added: first cut at import and data directory with decompressed zip

---
+++ run_analysis.R
@@ -0,0 +1,111 @@
+# You should create one R script called run_analysis.R that does the following. 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set
+#    with the average of each variable for each activity and each subject.
+
+
+# 1 WALKING
+# 2 WALKING_UPSTAIRS
+# 3 WALKING_DOWNSTAIRS
+# 4 SITTING
+# 5 STANDING
+# 6 LAYING
+
+library("data.table")
+
+cols<-c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,125,126,161,162,163,164,165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,426,427,428,429,503,504,516,517,529,530,542,543,555,556,557,558,559,560,561)
+
+col_names<-c("activity",
+             "tBodyAcc-mean()-X",
+             "tBodyAcc-mean()-Y",
+             "tBodyAcc-mean()-Z",
+             "tBodyAcc-std()-X",
+             "tBodyAcc-std()-Y",
+             "tBodyAcc-std()-Z",
+             "tGravityAcc-mean()-X",
+             "tGravityAcc-mean()-Y",
+             "tGravityAcc-mean()-Z",
+             "tGravityAcc-std()-X",
+             "tGravityAcc-std()-Y",
+             "tGravityAcc-std()-Z",
+             "tBodyAccJerk-mean()-X",
+             "tBodyAccJerk-mean()-Y",
+             "tBodyAccJerk-mean()-Z",
+             "tBodyAccJerk-std()-X",
+             "tBodyAccJerk-std()-Y",
+             "tBodyAccJerk-std()-Z",
+             "tBodyGyro-mean()-X",
+             "tBodyGyro-mean()-Y",
+             "tBodyGyro-mean()-Z",
+             "tBodyGyro-std()-X",
+             "tBodyGyro-std()-Y",
+             "tBodyGyro-std()-Z",
+             "tBodyGyroJerk-mean()-X",
+             "tBodyGyroJerk-mean()-Y",
+             "tBodyGyroJerk-mean()-Z",
+             "tBodyGyroJerk-std()-X",
+             "tBodyGyroJerk-std()-Y",
+             "tBodyGyroJerk-std()-Z",
+             "tBodyAccMag-mean()",
+             "tBodyAccMag-std()",
+             "tGravityAccMag-mean()",
+             "tGravityAccMag-std()",
+             "tBodyAccJerkMag-mean()",
+             "tBodyAccJerkMag-std()",
+             "tBodyGyroMag-mean()",
+             "tBodyGyroMag-std()",
+             "tBodyGyroJerkMag-mean()",
+             "tBodyGyroJerkMag-std()",
+             "fBodyAcc-mean()-X",
+             "fBodyAcc-mean()-Y",
+             "fBodyAcc-mean()-Z",
+             "fBodyAcc-std()-X",
+             "fBodyAcc-std()-Y",
+             "fBodyAcc-std()-Z",
+             "fBodyAccJerk-mean()-X",
+             "fBodyAccJerk-mean()-Y",
+             "fBodyAccJerk-mean()-Z",
+             "fBodyAccJerk-std()-X",
+             "fBodyAccJerk-std()-Y",
+             "fBodyAccJerk-std()-Z",
+             "fBodyGyro-mean()-X",
+             "fBodyGyro-mean()-Y",
+             "fBodyGyro-mean()-Z",
+             "fBodyGyro-std()-X",
+             "fBodyGyro-std()-Y",
+             "fBodyGyro-std()-Z",
+             "fBodyAccMag-mean()",
+             "fBodyAccMag-std()",
+             "fBodyBodyAccJerkMag-mean()",
+             "fBodyBodyAccJerkMag-std()",
+             "fBodyBodyGyroMag-mean()",
+             "fBodyBodyGyroMag-std()",
+             "fBodyBodyGyroJerkMag-mean()",
+             "fBodyBodyGyroJerkMag-std()",
+             "angle(tBodyAccMean,gravity)",
+             "angle(tBodyAccJerkMean),gravityMean)",
+             "angle(tBodyGyroMean,gravityMean)",
+             "angle(tBodyGyroJerkMean,gravityMean)",
+             "angle(X,gravityMean)",
+             "angle(Y,gravityMean)",
+             "angle(Z,gravityMean)")
+
+train_activity<-fread("./data/train/y_train.txt", header=FALSE)
+train_activity<-data.table(factor(train_activity$V1, levels=1:6, labels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")))
+
+train_set<-cbind(train_activity, fread("./data/train/X_train.txt", select=cols, header=FALSE))
+names(train_set) <- col_names
+
+
+# test_set<-fread("./data/test/X_train.txt", select=cols, header=FALSE)
+# names(test_set) <- col_names
+# full_set <- rbind(train_set,test_set)
+
+
+
+
+
+
